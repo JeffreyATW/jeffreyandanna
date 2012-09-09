316264e3609cd24618c3c2b7832d398afa692da7
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2012-09-08 18:11:19 -0700

    Narrowing page to 800 across

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -4,6 +4,14 @@ class Guest < ActiveRecord::Base
   attr_accessible :name, :special_needs, :invitation_id, :invitation_attributes
   validates_presence_of :name, :invitation
 
+  HUMANIZED_ATTRIBUTES = {
+    :special_needs => "Special requests"
+  }
+
+  def self.human_attribute_name(attr, options={})
+    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
+
   def to_s
     name
   end
