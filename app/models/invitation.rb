cc09cd0d36636c221d219137af7dc79f86b56cd7
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2013-02-28 17:16:41 -0800

    Making address name generation more complicated

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -25,28 +25,52 @@ class Invitation < ActiveRecord::Base
   end
 
   def name
-    #"#{guests.first}#{guests.length > 1 ? " + #{guests.length - 1}" : ""}"
+    "#{guests.first}#{guests.length > 1 ? " + #{guests.length - 1}" : ""}"
   end
 
   def address_name
     families = []
     last_names = {}
+    new_last_names = {}
     guests.each do |guest|
       # Discard if name has question marks
       unless guest.name =~ /\?/
         names = guest.name.split(' ')
-        # Discard if last name isn't uppercase
+        # Discard if last name isn't uppercase, it's a placeholder
         if names.last =~ /[A-Z]/
           # If last name ends with a period, it's a suffix
-          if names.last =~ /\.$/
+          if names.last =~ /\. *$/
             # Add to last name list for penultimate word
-            (last_names[names[-2]] ||= []) << names.first
+            (last_names[names[-2]] ||= []) << guest.name
           else
-            (last_names[names.last] ||= []) << names.first
+            (last_names[names.last] ||= []) << guest.name
           end
         end
       end
     end
+    last_names.each do |last_name, names|
+      hash_to_use = last_names
+      key_to_use = last_name
+      if names.length > 1
+        first_person_names = names.first.split(' ')
+        # If first person has more than two names
+        if first_person_names.length > 2
+          second_person_names = names[1].split(' ')
+          # Check to see if subsequent person has same penultimate name
+          if second_person_names.length > 2 && second_person_names[-2] == first_person_names[-2]
+            # Create new key for new last name
+            hash_to_use = new_last_names
+            key_to_use = first_person_names[-2] + ' ' + last_name
+            last_names.delete last_name
+          end
+        end
+        hash_to_use[key_to_use] = names.map{|name| name.split(' ').first}
+      else
+        # Store all words except last name, will be concatenated next
+        last_names[last_name] = [names.first.sub(/ [^ ]+ *$/, '')]
+      end
+    end
+    last_names.merge! new_last_names
     last_names.each do |last_name, first_names|
       if first_names.length > 4
         families << "The #{last_name} Family"
