fae5abe7a62ac53e4300243b720584aa939c1843
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2013-05-08 14:59:53 -0700

    Moving secret_token to app_config

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Jeffreyandanna::Application.config.secret_token = '7997648f1a1df4eb6629e77b5a64e276cf6f0b66e88ce25b76fddbac21281e0099998f9704f42f94b3dbf316bc0568b5649575fded4d53e7b37e8ca9119fe93a'
+
+Jeffreyandanna::Application.config.secret_token = APP_CONFIG['secret_token']
