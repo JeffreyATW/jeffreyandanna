11a5d895e311aecf0405861d3c59736f7852b981
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2013-05-08 14:01:00 -0700

    Adding date countdown

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @remaining = (Date.new(2013,8,17) - Date.today).to_i
     @feed = Feedzirra::Feed.fetch_and_parse("http://jeffreyandanna.us/blog/feed/")
     if session[:rsvp]
       @invitation = Invitation.find_by_rsvp(session[:rsvp])
