48f6cc49bf09afce02777938b9e3db18e4d4c434
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2013-05-23 18:51:52 -0700

    Adding tables to precompile

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module Jeffreyandanna
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.precompile += %w(tables.js)
+
     #config.assets.prefix = ""
   end
 end
