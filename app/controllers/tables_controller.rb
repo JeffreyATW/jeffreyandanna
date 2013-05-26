d5ebfba430ec46615c1e67034cf6bd45ebee0371
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2013-05-26 11:09:05 -0700

    Adding authenticate to tables controller

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,5 +1,7 @@
 require 'rabl'
 class TablesController < ApplicationController
+  before_filter :authenticate_user!
+
   Rabl.configure do |config|
     config.include_json_root = false
     config.include_child_root = false
