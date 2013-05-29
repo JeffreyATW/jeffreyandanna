b23e3be7ff894f240e560a733d68ef925d6d34ba
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2013-05-29 09:24:19 -0700

    Specifying sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Jeffreyandanna::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_url_options = { :host => 'jeffreyandanna.us' }
 
   config.assets.prefix = ''
