677350093e9930dce0060c55268d8ca5ce31c350
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2014-04-10 23:05:46 -0700

    Removing wp tables from schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,13 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130608235502) do
+ActiveRecord::Schema.define(version: 20130608235502) do
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0
+    t.integer  "attempts",   default: 0
     t.text     "handler"
     t.text     "last_error"
     t.datetime "run_at"
@@ -23,251 +23,93 @@ ActiveRecord::Schema.define(:version => 20130608235502) do
     t.datetime "failed_at"
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
-  create_table "guest_mails", :force => true do |t|
+  create_table "guest_mails", force: true do |t|
     t.string   "subject"
     t.text     "body"
     t.string   "group"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "guests", :force => true do |t|
+  create_table "guests", force: true do |t|
     t.string   "name"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "invitation_id"
     t.text     "special_needs"
     t.boolean  "under_4"
     t.integer  "table_id"
   end
 
-  create_table "invitations", :force => true do |t|
+  create_table "invitations", force: true do |t|
     t.text     "address"
     t.boolean  "responded"
     t.boolean  "going"
     t.string   "rsvp"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "email"
     t.text     "notes"
     t.boolean  "save_the_date_sent"
     t.boolean  "invited"
   end
 
-  create_table "rails_admin_histories", :force => true do |t|
+  create_table "rails_admin_histories", force: true do |t|
     t.text     "message"
     t.string   "username"
     t.integer  "item"
     t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
+    t.integer  "month",      limit: 2
+    t.integer  "year",       limit: 8
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
 
-  create_table "tables", :force => true do |t|
+  create_table "tables", force: true do |t|
     t.string   "name"
     t.text     "notes"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "x"
     t.integer  "y"
     t.string   "table_type"
   end
 
-  create_table "tasks", :force => true do |t|
+  create_table "tasks", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "creator_id"
     t.boolean  "done"
     t.integer  "parent_task_id"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "due_date"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-  create_table "wp_commentmeta", :primary_key => "meta_id", :force => true do |t|
-    t.integer "comment_id", :limit => 8,          :default => 0, :null => false
-    t.string  "meta_key"
-    t.text    "meta_value", :limit => 2147483647
-  end
-
-  add_index "wp_commentmeta", ["comment_id"], :name => "comment_id"
-  add_index "wp_commentmeta", ["meta_key"], :name => "meta_key"
-
-  create_table "wp_comments", :primary_key => "comment_ID", :force => true do |t|
-    t.integer  "comment_post_ID",      :limit => 8,   :default => 0,   :null => false
-    t.text     "comment_author",       :limit => 255,                  :null => false
-    t.string   "comment_author_email", :limit => 100, :default => "",  :null => false
-    t.string   "comment_author_url",   :limit => 200, :default => "",  :null => false
-    t.string   "comment_author_IP",    :limit => 100, :default => "",  :null => false
-    t.datetime "comment_date",                                         :null => false
-    t.datetime "comment_date_gmt",                                     :null => false
-    t.text     "comment_content",                                      :null => false
-    t.integer  "comment_karma",                       :default => 0,   :null => false
-    t.string   "comment_approved",     :limit => 20,  :default => "1", :null => false
-    t.string   "comment_agent",                       :default => "",  :null => false
-    t.string   "comment_type",         :limit => 20,  :default => "",  :null => false
-    t.integer  "comment_parent",       :limit => 8,   :default => 0,   :null => false
-    t.integer  "user_id",              :limit => 8,   :default => 0,   :null => false
-  end
-
-  add_index "wp_comments", ["comment_approved", "comment_date_gmt"], :name => "comment_approved_date_gmt"
-  add_index "wp_comments", ["comment_date_gmt"], :name => "comment_date_gmt"
-  add_index "wp_comments", ["comment_parent"], :name => "comment_parent"
-  add_index "wp_comments", ["comment_post_ID"], :name => "comment_post_ID"
-
-  create_table "wp_links", :primary_key => "link_id", :force => true do |t|
-    t.string   "link_url",                             :default => "",  :null => false
-    t.string   "link_name",                            :default => "",  :null => false
-    t.string   "link_image",                           :default => "",  :null => false
-    t.string   "link_target",      :limit => 25,       :default => "",  :null => false
-    t.string   "link_description",                     :default => "",  :null => false
-    t.string   "link_visible",     :limit => 20,       :default => "Y", :null => false
-    t.integer  "link_owner",       :limit => 8,        :default => 1,   :null => false
-    t.integer  "link_rating",                          :default => 0,   :null => false
-    t.datetime "link_updated",                                          :null => false
-    t.string   "link_rel",                             :default => "",  :null => false
-    t.text     "link_notes",       :limit => 16777215,                  :null => false
-    t.string   "link_rss",                             :default => "",  :null => false
-  end
-
-  add_index "wp_links", ["link_visible"], :name => "link_visible"
-
-  create_table "wp_login_security_solution_fail", :primary_key => "fail_id", :force => true do |t|
-    t.string    "ip",          :limit => 39, :default => "", :null => false
-    t.string    "user_login",  :limit => 60, :default => "", :null => false
-    t.string    "pass_md5",    :limit => 64, :default => "", :null => false
-    t.timestamp "date_failed",                               :null => false
-  end
-
-  add_index "wp_login_security_solution_fail", ["ip"], :name => "ip", :length => {"ip"=>9}
-  add_index "wp_login_security_solution_fail", ["pass_md5"], :name => "pass_md5", :length => {"pass_md5"=>10}
-  add_index "wp_login_security_solution_fail", ["user_login"], :name => "user_login", :length => {"user_login"=>5}
-
-  create_table "wp_options", :primary_key => "option_id", :force => true do |t|
-    t.string "option_name",  :limit => 64,         :default => "",    :null => false
-    t.text   "option_value", :limit => 2147483647,                    :null => false
-    t.string "autoload",     :limit => 20,         :default => "yes", :null => false
-  end
-
-  add_index "wp_options", ["option_name"], :name => "option_name", :unique => true
-
-  create_table "wp_postmeta", :primary_key => "meta_id", :force => true do |t|
-    t.integer "post_id",    :limit => 8,          :default => 0, :null => false
-    t.string  "meta_key"
-    t.text    "meta_value", :limit => 2147483647
-  end
-
-  add_index "wp_postmeta", ["meta_key"], :name => "meta_key"
-  add_index "wp_postmeta", ["post_id"], :name => "post_id"
-
-  create_table "wp_posts", :primary_key => "ID", :force => true do |t|
-    t.integer  "post_author",           :limit => 8,          :default => 0,         :null => false
-    t.datetime "post_date",                                                          :null => false
-    t.datetime "post_date_gmt",                                                      :null => false
-    t.text     "post_content",          :limit => 2147483647,                        :null => false
-    t.text     "post_title",                                                         :null => false
-    t.text     "post_excerpt",                                                       :null => false
-    t.string   "post_status",           :limit => 20,         :default => "publish", :null => false
-    t.string   "comment_status",        :limit => 20,         :default => "open",    :null => false
-    t.string   "ping_status",           :limit => 20,         :default => "open",    :null => false
-    t.string   "post_password",         :limit => 20,         :default => "",        :null => false
-    t.string   "post_name",             :limit => 200,        :default => "",        :null => false
-    t.text     "to_ping",                                                            :null => false
-    t.text     "pinged",                                                             :null => false
-    t.datetime "post_modified",                                                      :null => false
-    t.datetime "post_modified_gmt",                                                  :null => false
-    t.text     "post_content_filtered", :limit => 2147483647,                        :null => false
-    t.integer  "post_parent",           :limit => 8,          :default => 0,         :null => false
-    t.string   "guid",                                        :default => "",        :null => false
-    t.integer  "menu_order",                                  :default => 0,         :null => false
-    t.string   "post_type",             :limit => 20,         :default => "post",    :null => false
-    t.string   "post_mime_type",        :limit => 100,        :default => "",        :null => false
-    t.integer  "comment_count",         :limit => 8,          :default => 0,         :null => false
-  end
-
-  add_index "wp_posts", ["post_author"], :name => "post_author"
-  add_index "wp_posts", ["post_name"], :name => "post_name"
-  add_index "wp_posts", ["post_parent"], :name => "post_parent"
-  add_index "wp_posts", ["post_type", "post_status", "post_date", "ID"], :name => "type_status_date"
-
-  create_table "wp_term_relationships", :id => false, :force => true do |t|
-    t.integer "object_id",        :limit => 8, :default => 0, :null => false
-    t.integer "term_taxonomy_id", :limit => 8, :default => 0, :null => false
-    t.integer "term_order",                    :default => 0, :null => false
-  end
-
-  add_index "wp_term_relationships", ["term_taxonomy_id"], :name => "term_taxonomy_id"
-
-  create_table "wp_term_taxonomy", :primary_key => "term_taxonomy_id", :force => true do |t|
-    t.integer "term_id",     :limit => 8,          :default => 0,  :null => false
-    t.string  "taxonomy",    :limit => 32,         :default => "", :null => false
-    t.text    "description", :limit => 2147483647,                 :null => false
-    t.integer "parent",      :limit => 8,          :default => 0,  :null => false
-    t.integer "count",       :limit => 8,          :default => 0,  :null => false
-  end
-
-  add_index "wp_term_taxonomy", ["taxonomy"], :name => "taxonomy"
-  add_index "wp_term_taxonomy", ["term_id", "taxonomy"], :name => "term_id_taxonomy", :unique => true
-
-  create_table "wp_terms", :primary_key => "term_id", :force => true do |t|
-    t.string  "name",       :limit => 200, :default => "", :null => false
-    t.string  "slug",       :limit => 200, :default => "", :null => false
-    t.integer "term_group", :limit => 8,   :default => 0,  :null => false
-  end
-
-  add_index "wp_terms", ["name"], :name => "name"
-  add_index "wp_terms", ["slug"], :name => "slug", :unique => true
-
-  create_table "wp_usermeta", :primary_key => "umeta_id", :force => true do |t|
-    t.integer "user_id",    :limit => 8,          :default => 0, :null => false
-    t.string  "meta_key"
-    t.text    "meta_value", :limit => 2147483647
-  end
-
-  add_index "wp_usermeta", ["meta_key"], :name => "meta_key"
-  add_index "wp_usermeta", ["user_id"], :name => "user_id"
-
-  create_table "wp_users", :primary_key => "ID", :force => true do |t|
-    t.string   "user_login",          :limit => 60,  :default => "", :null => false
-    t.string   "user_pass",           :limit => 64,  :default => "", :null => false
-    t.string   "user_nicename",       :limit => 50,  :default => "", :null => false
-    t.string   "user_email",          :limit => 100, :default => "", :null => false
-    t.string   "user_url",            :limit => 100, :default => "", :null => false
-    t.datetime "user_registered",                                    :null => false
-    t.string   "user_activation_key", :limit => 60,  :default => "", :null => false
-    t.integer  "user_status",                        :default => 0,  :null => false
-    t.string   "display_name",        :limit => 250, :default => "", :null => false
-  end
-
-  add_index "wp_users", ["user_login"], :name => "user_login_key"
-  add_index "wp_users", ["user_nicename"], :name => "user_nicename"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
