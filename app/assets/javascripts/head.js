e1f8cfa6ed9032adf4eede22d1b4b09e5935f969
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2014-11-06 21:13:04 -0800

    Prepping site for launch

---
+++ app/assets/javascripts/head.js
@@ -0,0 +1,6 @@
+/*
+ *= require libs/modernizr
+ *= require angular
+ *= require angular-route
+ *= require angular-animate
+*/
--- config/routes.rb
+++ config/routes.rb
@@ -62,9 +62,8 @@ Adalandlily::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  get 'preview' => 'welcome#index'
 
-  root :to => 'welcome#coming_soon'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
