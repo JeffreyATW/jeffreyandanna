e2cdf41de3504cfd62134424479ffaf9ed0d0cb6
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2016-01-01 22:17:36 -0800

    Allowing iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  after_action :allow_iframe
+  
+  private
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
 end
