6767a349edc1b270eb100491012b1dc28b5e0481
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2017-04-21 17:20:11 -0700

    Change server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'web308.webfaction.com', user: 'jeffreyatw', roles: %w{app db web}
+server 'web520.webfaction.com', user: 'jeffreyatw', roles: %w{app db web}
 
 
 
